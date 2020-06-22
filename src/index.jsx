0d6a51a1f12e955b2d989a9bfa89d18258d7bb1f
listingslab <listingslab@gmail.com>
2020-06-23 09:32:11 +1000

    Strip CRA

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PushToTalk from './PushToTalk'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PushToTalk />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
