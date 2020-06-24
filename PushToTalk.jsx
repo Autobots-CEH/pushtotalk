7b2dcf8fa5224ec86547c4a263fa90ab62fe528a
weizang-75 <weizang75@gmail.com>
2020-06-25 07:59:07 +1000

    添加版本文件

--- PushToTalk.jsx
+++ PushToTalk.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 
 export default function PushToTalk(props) {
+	console.log ('PushToTalk verified')
 	return (
 		<div style={{ border: '1px solid red' }}>
 			<h1>PushToTalk</h1>
